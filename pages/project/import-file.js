0d367a19a69b125bdae9d56c172aa959744101e8
xak2 <v.stefanovich@inbox.lv>
2020-06-04 07:58:54 +0300

    Changes in import lib

--- pages/project/import-file.js
+++ pages/project/import-file.js
@@ -21,32 +21,44 @@ export default class ImportFile extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = { hideDialog: true, name: '', designation: '', error: undefined }
+        this.state = { hideDialog: true, name: '', selectedFile: '', error: undefined }
     }
 
     _showDialog = () => { this.setState({ hideDialog: false }) }
-    _closeDialog = () => { this.setState({ hideDialog: true, name: '', designation: '', error: undefined }) }
+
+    _closeDialog = () => { this.setState({ hideDialog: true, name: '', selectedFile: '', error: undefined }) }
+
     _handleChange = (event) => {
         const target = event.target
         const fieldValue = target.value
         const fieldName = target.name
         if (fieldName === 'name') this.setState({ name: fieldValue })
-        else if (fieldName === 'designation') this.setState({ designation: fieldValue })
+        else if (fieldName === 'upload_file') this.setState({ selectedFile: event.target.files[0] })
+        console.log(this.state)
     }
+
     _handleSubmit = () => {
+
         var self = this
+        const data = new FormData()
+        data.append('file', this.state.selectedFile)
+        data.append('name', this.state.name)
+        data.append('pid', this.props.pid)
+
         axios.post(
             'http://localhost/framehouse-app/php/projects.php?action=create',
-            { name: this.state.name, designation: this.state.designation, cid: this.props.cid },
+            data,
             { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
         ).then((response) => {
             console.log(response)
+/*
             if (response.data.error) {
                 self.setState({ error: response.data.error.join(' ') })
             } else if (response.data.success === true) {
                 self.setState({ hideDialog: true, name: '', designation: '', error: undefined })
                 this.props.handler()
             }
+*/
         })
     }
 
@@ -73,7 +85,7 @@ export default class ImportFile extends React.Component {
                     {error ? ErrorBar : ''}
                     <TextField name="name" onChange={this._handleChange} label="Group name" iconProps={{ iconName: 'Rename' }} />
                     <Stack horizontal tokens={{ padding: '10px 0' }}><Text variant='medium' styles={{ root: { fontWeight: 600 } }}>Select single BVN or ASC file for import</Text></Stack>
-                    <input type="file" className="form-control" name="upload_file" onChange={this.handleInputChange} style={{ margin: '5px 0', fontSize: '14px', width: 300}} />
+                    <input type="file" className="form-control" name="upload_file" onChange={this._handleChange} style={{ margin: '5px 0', fontSize: '14px', width: 300 }} />
                     <DialogFooter>
                         <PrimaryButton onClick={this._handleSubmit} iconProps={{ iconName: 'CloudImportExport' }} text="Import" />
                         <DefaultButton onClick={this._closeDialog} iconProps={{ iconName: 'Cancel' }} text="Cancel" />
